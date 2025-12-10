7fa16fe09dbd0e4f3c8ec6443ca43dc0de494e7f
sandyip <sandipyadav0556@gmail.com>
2025-12-10 21:10:19 +0545

    Dynamic cloud- community section and upcoming events section

--- wp-content/themes/ncp/inc/resources.php
+++ wp-content/themes/ncp/inc/resources.php
@@ -124,3 +124,63 @@ function add_additional_class_on_a($atts, $item, $args) {
     return $atts;
 }
 add_filter('nav_menu_link_attributes', 'add_additional_class_on_a', 10, 3);
+
+// Event custom post type
+  
+function event_custom_post_type() {
+  
+// Set UI labels for Custom Post Type
+    $labels = array(
+        'name'                => _x( 'Our Events', 'Post Type General Name', 'ncp' ),
+        'singular_name'       => _x( 'Our Event', 'Post Type Singular Name', 'ncp' ),
+        'menu_name'           => __( 'Our Events', 'ncp' ),
+        'parent_item_colon'   => __( 'Parent Our Event', 'ncp' ),
+        'all_items'           => __( 'All Our Events', 'ncp' ),
+        'view_item'           => __( 'View Our Event', 'ncp' ),
+        'add_new_item'        => __( 'Add New Our Event', 'ncp' ),
+        'add_new'             => __( 'Add New', 'ncp' ),
+        'edit_item'           => __( 'Edit Our Event', 'ncp' ),
+        'update_item'         => __( 'Update Our Event', 'ncp' ),
+        'search_items'        => __( 'Search Our Event', 'ncp' ),
+        'not_found'           => __( 'Not Found', 'ncp' ),
+        'not_found_in_trash'  => __( 'Not found in Trash', 'ncp' ),
+    );
+      
+// Set other options for Custom Post Type
+      
+    $args = array(
+        'label'               => __( 'our events', 'ncp' ),
+        'description'         => __( 'Our Events news and reviews', 'ncp' ),
+        'labels'              => $labels,
+        // Features this CPT supports in Post Editor
+        'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+        // You can associate this CPT with a taxonomy or custom taxonomy. 
+        'taxonomies'          => array( 'genres' ),
+        /* A hierarchical CPT is like Pages and can have
+        * Parent and child items. A non-hierarchical CPT
+        * is like Posts.
+        */
+        'hierarchical'        => false,
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'show_in_nav_menus'   => true,
+        'show_in_admin_bar'   => true,
+        'menu_position'       => 5,
+        'menu_icon'           => 'dashicons-media-archive',
+        'can_export'          => true,
+        'has_archive'         => true,
+        'exclude_from_search' => false,
+        'publicly_queryable'  => true,
+        'capability_type'     => 'post',
+        'show_in_rest' => true,
+  
+    );
+      
+    // Registering your Custom Post Type
+    register_post_type( 'our_events', $args );
+  
+}
+
+  
+add_action( 'init', 'event_custom_post_type', 0 );
--- wp-content/themes/ncp/template-parts/homepage/cloud-community.php
+++ wp-content/themes/ncp/template-parts/homepage/cloud-community.php
@@ -3,59 +3,128 @@
     <div class="row justify-content-center">
       <div class="col-lg-10">
         <div class="section-title">
-          <h2 class="text-40 mb-32 leading-130">Building Nepal’s Cloud Community</h2>
-          <p class="text-20 text-title leading-150">Since 2017, Nepal Cloud Professionals has been Nepal’s leading
-            not-for-profit
-            community for IT
-            professionals,
-            developers, and cloud enthusiasts. We empower our members through:</p>
+          <!-- Section title -->
+          <?php if ($section_title = get_sub_field('section_title')) : ?>
+            <h2 class="text-40 mb-32 leading-130"><?php echo esc_html($section_title); ?></h2>
+          <?php endif; ?>
+          <!-- !! section title -->
+
+          <!-- section description -->
+          <?php if ($section_description = get_sub_field('section_description')) : ?>
+            <p class="text-20 text-title leading-150"><?php echo $section_description; ?></p>
+          <?php endif; ?>
+          <!-- !! section description -->
         </div>
       </div>
 
-      <div class="col-lg-5 col-sm-6">
-        <div class="cloud-community-card px-24 pb-24 pt-32 bg-accent rounded-16">
-          <div class="d-flex gap-16 mb-16 align-items-center">
-            <div class="cloud-community-icon">
-              <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/laptop.svg" alt="" class="img-fluid" />
+      <!-- cloud community card 1 -->
+      <?php if (have_rows('cloud_community_card1')) : ?>
+        <?php while (have_rows('cloud_community_card1')) :
+          the_row();
+          $card1_icon = get_sub_field('card1_icon');
+          $title = get_sub_field('title');
+          $description = get_sub_field('description');
+          $card1_image = get_sub_field('card1_image');
+        ?>
+          <div class="col-lg-5 col-sm-6">
+            <div class="cloud-community-card px-24 pb-24 pt-32 bg-accent rounded-16">
+              <div class="d-flex gap-16 mb-16 align-items-center">
+                <!-- icon -->
+                <?php if ($card1_icon) : ?>
+                  <div class="cloud-community-icon">
+                    <img src="<?php echo esc_url($card1_icon['url']); ?>" alt="<?php echo esc_attr($card1_icon['alt']); ?>" class="img-fluid" />
+                  </div>
+                <?php endif; ?>
+                <!-- !!icon -->
+
+                <!-- title -->
+                <?php if ($title): ?>
+                  <h3 class="text-24 fw-600 text-title leading-150"><?php echo esc_html($title); ?></h3>
+                <?php endif; ?>
+                <!-- !!title -->
+              </div>
+
+              <!-- description -->
+              <?php if ($description) : ?>
+                <p class="mb-32 text-title leading-150"><?php echo $description; ?></p>
+              <?php endif; ?>
+              <!-- !! description -->
+
+              <!-- image -->
+              <?php if ($card1_image) : ?>
+                <div class="cloud-community-img rounded-4">
+                  <img src="<?php echo esc_url($card1_image['url']); ?>" alt="<?php echo esc_attr($card1_image['alt']); ?>"
+                    class="img-fluid" />
+                </div>
+
+              <?php endif; ?>
+              <!-- !image -->
             </div>
-            <h3 class="text-24 fw-600 text-title leading-150">Workshops</h3>
           </div>
-          <p class="mb-32 text-title leading-150">Hands-on sessions on Microsoft Azure, Power BI, PowerApps, Dynamics
-            365, and Cloud
-            Security
-            to upskill and
-            innovate. Our workshops provide practical, real-world projects guided by experts to boost your career.</p>
-          <div class="cloud-community-img rounded-4">
-            <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/cloud-community1.png" alt=""
-              class="img-fluid" />
-          </div>
-        </div>
-      </div>
-      <div class="col-lg-5 col-sm-6">
-        <div class="cloud-community-card px-24 pb-24 pt-32 bg-accent rounded-16">
-          <div class="d-flex gap-16 mb-16 align-items-center">
-            <div class="cloud-community-icon">
-              <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/coworking.svg" alt=""
-                class="img-fluid" />
+        <?php endwhile; ?>
+      <?php endif; ?>
+      <!-- !! cloud community card 1 -->
+
+      <!-- cloud community card 2 -->
+      <?php if (have_rows('cloud_community_card2')) : ?>
+        <?php while (have_rows('cloud_community_card2')) :
+          the_row();
+          $card2_icon = get_sub_field('card2_icon');
+          $title = get_sub_field('title');
+          $description = get_sub_field('description');
+          $card2_image = get_sub_field('card2_image');
+        ?>
+          <div class="col-lg-5 col-sm-6">
+            <div class="cloud-community-card px-24 pb-24 pt-32 bg-accent rounded-16">
+              <div class="d-flex gap-16 mb-16 align-items-center">
+                <!-- icon -->
+                <?php if ($card2_icon) : ?>
+                  <div class="cloud-community-icon">
+                    <img src="<?php echo esc_url($card2_icon['url']); ?>" alt="<?php echo esc_attr($card2_icon['alt']); ?>"
+                      class="img-fluid" />
+                  </div>
+                <?php endif; ?>
+                <!-- !!icon -->
+                <!-- title -->
+                <?php if ($title): ?>
+                  <h3 class="text-24 fw-600 text-title leading-150"><?php echo esc_html($title); ?></h3>
+                <?php endif; ?>
+                <!-- !!title -->
+              </div>
+              <!-- description -->
+              <?php if ($description) : ?>
+                <p class="mb-32 text-title leading-150"><?php echo $description; ?></p>
+              <?php endif; ?>
+              <!-- !! description -->
+
+              <!-- image -->
+              <?php if ($card2_image) : ?>
+                <div class="cloud-community-img rounded-4">
+                  <img src="<?php echo esc_url($card2_image['url']); ?>" alt="<?php echo esc_attr($card2_image['alt']); ?>"
+                    class="img-fluid" />
+                </div>
+              <?php endif; ?>
+              <!-- !image -->
             </div>
-            <h3 class="text-24 fw-600 text-title leading-150">Meetups</h3>
           </div>
-          <p class="mb-32 text-title leading-150">Hands-on sessions on Microsoft Azure, Power BI, PowerApps, Dynamics
-            365, and Cloud
-            Security
-            to upskill and
-            innovate. Our workshops provide practical, real-world projects guided by experts to boost your career.</p>
-          <div class="cloud-community-img rounded-4">
-            <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/cloud-community2.png" alt=""
-              class="img-fluid" />
-          </div>
+        <?php endwhile; ?>
+      <?php endif; ?>
+      <!-- !! cloud community card 2 -->
+
+      <!-- button -->
+
+      <?php
+      $link = get_sub_field('button');
+      if ($link) :
+        $link_url = $link['url'];
+        $link_title = $link['title'];
+        $link_target = $link['target'] ? $link['target'] : '_self';
+      ?>
+        <div class="col-lg-10">
+          <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>" class="py-12 px-36 text-white bg-primary rounded-48"><?php echo esc_html($link_title); ?></a>
         </div>
-      </div>
-
-      <div class="col-lg-10">
-        <a href="#" class="py-12 px-36 text-white bg-primary rounded-48">Learn More About Us</a>
-      </div>
-
+      <?php endif; ?>
+      <!-- !!button -->
     </div>
   </div>
 </section>
