a8c49fc27d95aa1e886654ab635e76a9cd5c7d1a
sandyip <sandipyadav0556@gmail.com>
2025-12-10 21:53:38 +0545

    Dynamic testimonial section--updated

--- wp-content/themes/ncp/template-parts/homepage/testimonial.php
+++ wp-content/themes/ncp/template-parts/homepage/testimonial.php
@@ -3,14 +3,14 @@
     <?php if (have_rows('testimonials')) : ?>
       <div class="testimonial-slider">
 
+        <!-- Slider main -->
         <div class="slider-for mb-48">
           <?php while (have_rows('testimonials')) :
             the_row();
-            $name = get_sub_field('name');
-            $company_name = get_sub_field('company_name');
             $quote_text = get_sub_field('quote_text');
-            $image = get_sub_field('image');
+            $author = get_sub_field('author_details');
           ?>
+
             <div class="testimonial-card h-100">
               <div class="quote-icon mb-16">
                 <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/quote.svg" alt="" class="img-fluid" />
@@ -19,17 +19,22 @@
                 <p class="mb-64 text-28 leading-140 fw-700"><?php echo $quote_text; ?></p>
               <?php endif; ?>
 
-              <?php if ($name && $company_name): ?>
-                <p class="text-center"><?php echo esc_html($name); ?>,<span class="text-gray"> <?php echo esc_html($company_name); ?></span></p>
+              <?php if (!empty($author['name']) || !empty($author['company_name'])): ?>
+                <p class="text-center"><?php echo esc_html($author['name']); ?>,<span class="text-gray"> <?php echo esc_html($author['company_name']); ?></span></p>
               <?php endif; ?>
             </div>
           <?php endwhile; ?>
         </div>
 
+        <!-- Slider thumb -->
         <div class="slider-nav">
           <?php while (have_rows('testimonials')) :
             the_row(); ?>
-            <?php if ($image) : ?>
+            <?php
+            $author = get_sub_field('author_details');
+            $image = $author['image'];
+            ?>
+            <?php if (!empty($image)): ?>
               <div class="testimonial-img">
                 <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>"
                   class="img-fluid" />
