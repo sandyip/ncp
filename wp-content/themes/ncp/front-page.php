3e9c3bf560600e8da76d34d90d058602a60ddc6e
sandyip <sandipyadav0556@gmail.com>
2025-12-10 17:01:16 +0545

    Dynamic banner section in frontpage

--- wp-content/themes/ncp/front-page.php
+++ wp-content/themes/ncp/front-page.php
@@ -1,6 +1,16 @@
 <?php
-get_header();
+get_header(); ?>
 
+<?php
+$flexibleContentPath = dirname(__FILE__) . '/flexible-content/';
+if ( have_rows( 'front_page_content' ) ) :
+	while ( have_rows( 'front_page_content' ) ) :
+		the_row();
+		$layout = get_row_layout();
+		$file = ( $flexibleContentPath . str_replace( '_', '-', $layout) . '.php' );
+		if ( file_exists( $file ) ) {
+			include( $file );
+		} 
 get_template_part('template-parts/banner', null);
 get_template_part('template-parts/homepage/cloud-community', null);
 get_template_part('template-parts/homepage/upcoming-events', null);
@@ -8,8 +18,9 @@ get_template_part('template-parts/homepage/testimonial', null);
 get_template_part('template-parts/homepage/join-conversation', null);
 get_template_part('template-parts/homepage/cloud-cta', null);
 get_template_part('template-parts/homepage/partners', null);
-?>
 
+endwhile;
+endif; ?>
 <?php
 get_footer();
 ?>
--- wp-content/themes/ncp/template-parts/banner.php
+++ wp-content/themes/ncp/template-parts/banner.php
@@ -4,35 +4,114 @@
       <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/banner-bat.svg" alt="" class="img-fluid" />
     </div>
 
-    <div class="left-img">
-      <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/banner-img1.svg" alt="" class="img-fluid" />
-    </div>
-    <div class="right-img d-none d-lg-block">
-      <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/banner-img2.svg" alt="" class="img-fluid" />
-    </div>
+    <!-- banner left image -->
+    <?php if (have_rows('banner_left_image')) : ?>
+      <?php while (have_rows('banner_left_image')) :
+        the_row(); ?>
+        <?php
+        $left_image = get_sub_field('left_image');
+        if ($left_image) : ?>
+          <div class="left-img">
+            <img src="<?php echo esc_url($left_image['url']); ?>" alt="<?php echo esc_attr($left_image['alt']); ?>" class="img-fluid" />
+          </div>
+        <?php endif; ?>
+      <?php endwhile; ?>
+    <?php endif; ?>
+    <!-- !! banner left image -->
+
+    <!-- banner right image -->
+    <?php if (have_rows('banner_right_image')) : ?>
+      <?php while (have_rows('banner_right_image')) :
+        the_row(); ?>
+        <?php
+        $right_image = get_sub_field('right_image');
+        if ($right_image) : ?>
+          <div class="right-img d-none d-lg-block">
+            <img src="<?php echo esc_url($right_image['url']); ?>" alt="<?php echo esc_attr($right_image['alt']); ?>" class="img-fluid" />
+          </div>
+        <?php endif; ?>
+      <?php endwhile; ?>
+    <?php endif; ?>
+    <!-- !! banner right image -->
 
     <div class="container">
-      <span class="banner-tag d-block py-4 px-60 text-uppercase text-white text-12 fw-700 text-center mb-8">learn,
-        connect, and innovate</span>
-      <h1 class="text-64 mb-24 text-center leading-130">Empowering Nepal’s Cloud Future</h1>
-      <p class="banner-content text-white mb-24 text-center fw-400 leading-150">Welcome to Nepal Cloud Professionals,
-        your hub for
-        cloud computing
-        excellence. Discover cutting-edge
-        technologies, network with industry leaders, and shape the future of IT in Nepal.</p>
-      <div class="join-btn d-flex justify-content-center">
-        <a href="#"
-          class="bg-primary-light px-36 py-12 text-white rounded-48 mb-32 hover-bg-white hover-text-primary-light">Join
-          Us</a>
-      </div>
-      <div class="rating d-flex align-items-center justify-content-center pb-92 gap-8">
-        <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/star.svg" alt="" class="img-fluid" />
-        <p class="text-white leading-150"> 4.2 from <span class="text-decoration-underline leading-150">48 rating
-            on</span> </p>
-        <a href="#">
-          <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/meetup.svg" alt="" class="img-fluid" />
-        </a>
-      </div>
+
+      <?php if (have_rows('banner_content_')) : ?>
+        <?php while (have_rows('banner_content_')) :
+          the_row(); ?>
+          <!-- Banner sub heading -->
+          <?php if ($banner_tagline = get_sub_field('banner_tagline')) : ?>
+            <span class="banner-tag d-block py-4 px-60 text-uppercase text-white text-12 fw-700 text-center mb-8"><?php echo esc_html($banner_tagline); ?></span>
+          <?php endif; ?>
+          <!-- !!! Banner sub heading -->
+
+          <!-- Banner heading -->
+          <?php if ($banner_heading = get_sub_field('banner_heading')) : ?>
+            <h1 class="text-64 mb-24 text-center leading-130"><?php echo esc_html($banner_heading); ?></h1>
+          <?php endif; ?>
+          <!-- !!! Banner heading -->
+
+          <!-- Banner text -->
+          <?php if ($banner_description = get_sub_field('banner_description')) : ?>
+            <p class="banner-content text-white mb-24 text-center fw-400 leading-150"><?php echo esc_html($banner_description); ?></p>
+          <?php endif; ?>
+          <!-- !!! Banner text -->
+
+
+          <!-- Bnner button -->
+          <?php
+          $link = get_sub_field('banner_button');
+          if ($link) :
+            $link_url = $link['url'];
+            $link_title = $link['title'];
+            $link_target = $link['target'] ? $link['target'] : '_self';
+          ?>
+            <div class="join-btn d-flex justify-content-center">
+              <a href="<?php echo esc_url($link_url); ?>"
+                class="bg-primary-light px-36 py-12 text-white rounded-48 mb-32 hover-bg-white hover-text-primary-light" target="<?php echo esc_attr($link_target); ?>"><?php echo esc_html($link_title); ?></a>
+            </div>
+          <?php endif; ?>
+          <!-- !!! Bnner button -->
+
+        <?php endwhile; ?>
+      <?php endif; ?>
+
+      <!-- Banner ratings and meetup -->
+      <?php if (have_rows('rating')) : ?>
+        <?php while (have_rows('rating')) :
+          the_row();
+          $rating_star_image = get_sub_field('rating_star_image');
+          $rating_number = get_sub_field('rating_number');
+          $rating_count = get_sub_field('rating_count');
+          $meetup_image = get_sub_field('meetup_image');
+          $link = get_sub_field('meetup_link');
+          $link_url = is_array($link) ? $link['url'] : $link;
+          $link_target = (is_array($link) && $link['target']) ? $link['target'] : '_self';
+
+        ?>
+
+          <?php if (($rating_star_image && $rating_count && $rating_number) || ($meetup_image && $link && is_array($link))) : ?>
+
+            <div class="rating d-flex align-items-center justify-content-center pb-92 gap-8">
+              <!-- rating star image -->
+              <img src="<?php echo esc_url($rating_star_image['url']); ?>" alt="<?php echo esc_attr($rating_star_image['alt']); ?>" class="img-fluid" />
+              <!-- !!! rating star image -->
+
+              <!-- rating count and rating number -->
+              <p class="text-white leading-150"> <?php echo esc_html($rating_number); ?> <span class="text-decoration-underline leading-150"><?php echo esc_html($rating_count); ?></span> </p>
+              <!-- !!! rating count and rating number -->
+
+              <!-- meetup -->
+              <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>">
+                <img src="<?php echo esc_url($meetup_image['url']); ?>" alt="<?php echo esc_attr($meetup_image['alt']); ?>" class="img-fluid" />
+              </a>
+              <!-- !!! meetup -->
+            </div>
+          <?php endif; ?>
+
+        <?php endwhile; ?>
+      <?php endif; ?>
+      <!--!!! Banner ratings and meetup -->
 
       <a href="#" class="d-flex justify-content-center">
         <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/go-down.svg" alt="" class="img-fluid" />
