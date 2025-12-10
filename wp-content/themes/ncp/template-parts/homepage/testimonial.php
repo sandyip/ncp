cb61e89a72b260f5bc1ceb368311673b49a2257c
sandyip <sandipyadav0556@gmail.com>
2025-12-10 21:33:48 +0545

    Dynamic testimonial section

--- wp-content/themes/ncp/template-parts/homepage/testimonial.php
+++ wp-content/themes/ncp/template-parts/homepage/testimonial.php
@@ -1,117 +1,45 @@
 <section class="testimonial bg-background py-lg-96 py-64">
   <div class="container">
-    <div class="testimonial-slider">
-      <div class="slider-for mb-48">
-        <div class="testimonial-card h-100">
-          <div class="quote-icon mb-16">
-            <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/quote.svg" alt="" class="img-fluid" />
-          </div>
-          <p class="mb-64 text-28 leading-140 fw-700">The Nepal Cloud Summit opened my eyes to <span>Azure’s
-              potential</span>, transforming how I <span>approach
-              cloud solutions in my
-              projects.</span> The <span>networking opportunities</span> were incredible, connecting me with experts who
-            inspired my career!</p>
-          <p class="text-center">Priya Sharma,<span class="text-gray"> Cloud Developer</span></p>
-        </div>
-        <div class="testimonial-card h-100">
-          <div class="quote-icon mb-16">
-            <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/quote.svg" alt="" class="img-fluid" />
-          </div>
-          <p class="mb-64 text-28 leading-140 fw-700">The Nepal Cloud Summit opened my eyes to <span>Azure’s
-              potential</span>, transforming how I <span>approach
-              cloud solutions in my
-              projects.</span> The were incredible, connecting me with experts who
-            inspired my career!</p>
-          <p class="text-center">Nirja Sharma,<span class="text-gray"> Cloud Developer</span></p>
-        </div>
-        <div class="testimonial-card h-100">
-          <div class="quote-icon mb-16">
-            <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/quote.svg" alt="" class="img-fluid" />
-          </div>
-          <p class="mb-64 text-28 leading-140 fw-700">The Nepal Cloud Summit opened my eyes to <span>Azure’s
-              potential</span>, transforming how I <span>approach
-              cloud solutions in my
-              projects.</span> The were incredible, connecting me with experts who
-            inspired my career!</p>
-          <p class="text-center">Nirja Sharma,<span class="text-gray"> Cloud Developer</span></p>
-        </div>
-        <div class="testimonial-card h-100">
-          <div class="quote-icon mb-16">
-            <img src="<?php echo get_parent_theme_file_uri() ?>/assets/imastyes/quote.svg" alt="" class="img-fluid" />
-          </div>
-          <p class="mb-64 text-28 leading-140 fw-700">The Nepal Cloud Summit opened my eyes to <span>Azure’s
-              potential</span>, transforming how I <span>approach
-              cloud solutions in my
-              projects.</span> The were incredible, connecting me with experts who
-            inspired my career!</p>
-          <p class="text-center">Puja Sharma,<span class="text-gray"> Cloud Developer</span></p>
-        </div>
-        <div class="testimonial-card h-100">
-          <div class="quote-icon mb-16">
-            <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/quote.svg" alt="" class="img-fluid" />
-          </div>
-          <p class="mb-64 text-28 leading-140 fw-700">The Nepal Cloud Summit opened my eyes to <span>Azure’s
-              potential</span>, transforming how I <span>approach
-              cloud solutions in my
-              projects.</span> The were incredible, connecting me with experts who
-            inspired my career!</p>
-          <p class="text-center">Pratibha Sharma,<span class="text-gray"> Cloud Developer</span></p>
-        </div>
-        <div class="testimonial-card h-100">
-          <div class="quote-icon mb-16">
-            <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/quote.svg" alt="" class="img-fluid" />
-          </div>
-          <p class="mb-64 text-28 leading-140 fw-700">The Nepal Cloud Summit opened my eyes to <span>Azure’s
-              potential</span>, transforming how I <span>approach
-              cloud solutions in my
-              projects.</span> The were incredible, connecting me with experts who
-            inspired my career!</p>
-          <p class="text-center">Supriya Sharma,<span class="text-gray"> Cloud Developer</span></p>
-        </div>
-        <div class="testimonial-card h-100">
-          <div class="quote-icon mb-16">
-            <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/quote.svg" alt="" class="img-fluid" />
-          </div>
-          <p class="mb-64 text-28 leading-140 fw-700">The Nepal Cloud Summit opened my eyes to <span>Azure’s
-              potential</span>, transforming how I <span>approach
-              cloud solutions in my
-              projects.</span> The were incredible, connecting me with experts who
-            inspired my career!</p>
-          <p class="text-center">Shiwangi Sharma,<span class="text-gray"> Cloud Developer</span></p>
-        </div>
-      </div>
+    <?php if (have_rows('testimonials')) : ?>
+      <div class="testimonial-slider">
 
-      <div class="slider-nav">
-        <div class="testimonial-img">
-          <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/testimonial-img1.png" alt=""
-            class="img-fluid" />
-        </div>
-        <div class="testimonial-img">
-          <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/testimonial-img1.png" alt=""
-            class="img-fluid" />
-        </div>
-        <div class="testimonial-img">
-          <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/testimonial-img1.png" alt=""
-            class="img-fluid" />
-        </div>
-        <div class="testimonial-img">
-          <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/testimonial-img1.png" alt=""
-            class="img-fluid" />
-        </div>
-        <div class="testimonial-img">
-          <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/testimonial-img1.png" alt=""
-            class="img-fluid" />
-        </div>
-        <div class="testimonial-img">
-          <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/testimonial-img1.png" alt=""
-            class="img-fluid" />
-        </div>
-        <div class="testimonial-img">
-          <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/testimonial-img1.png" alt=""
-            class="img-fluid" />
-        </div>
-      </div>
+        <div class="slider-for mb-48">
+          <?php while (have_rows('testimonials')) :
+            the_row();
+            $name = get_sub_field('name');
+            $company_name = get_sub_field('company_name');
+            $quote_text = get_sub_field('quote_text');
+            $image = get_sub_field('image');
+          ?>
+            <div class="testimonial-card h-100">
+              <div class="quote-icon mb-16">
+                <img src="<?php echo get_parent_theme_file_uri() ?>/assets/images/quote.svg" alt="" class="img-fluid" />
+              </div>
+              <?php if ($quote_text): ?>
+                <p class="mb-64 text-28 leading-140 fw-700"><?php echo $quote_text; ?></p>
+              <?php endif; ?>
 
-    </div>
+              <?php if ($name && $company_name): ?>
+                <p class="text-center"><?php echo esc_html($name); ?>,<span class="text-gray"> <?php echo esc_html($company_name); ?></span></p>
+              <?php endif; ?>
+            </div>
+          <?php endwhile; ?>
+        </div>
+
+        <div class="slider-nav">
+          <?php while (have_rows('testimonials')) :
+            the_row(); ?>
+            <?php if ($image) : ?>
+              <div class="testimonial-img">
+                <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>"
+                  class="img-fluid" />
+              </div>
+            <?php endif; ?>
+          <?php endwhile; ?>
+
+        </div>
+
+      </div>
+    <?php endif; ?>
   </div>
 </section>
