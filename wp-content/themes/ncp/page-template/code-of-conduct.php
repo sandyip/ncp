0fd42d185dee388a675a91ca8f512f7117262e13
sandyip <sandipyadav0556@gmail.com>
2025-12-11 16:00:55 +0545

    Dynamic code  of conduct page

--- wp-content/themes/ncp/page-template/code-of-conduct.php
+++ wp-content/themes/ncp/page-template/code-of-conduct.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template Name: Code of Conduct
  */
@@ -9,66 +10,52 @@ get_header();
 <section class="code-of-conduct py-lg-96 py-64">
   <div class="container">
     <div class="section-title text-center mb-48">
-      <p class="page-name mb-4 text-primary-light">Home / Code of Conduct</p>
+      <!-- <p class="page-name mb-4 text-primary-light">Home / Code of Conduct</p>  -->
+      <?php ncp_breadcrumb(); ?>
 
-      <h1 class="text-64 mb-24 leading-120">Code of Conduct</h1>
-      <p>At Nepal Cloud Professionals, we believe in creating a community where everyone can learn, collaborate, and
-        innovate without fear of harassment or discrimination. Our Code of Conduct outlines the standards we uphold at
-        our
-        events, meetups, and online platforms to ensure a positive experience for all participants.</p>
+      <!-- banner title -->
+      <?php if ($banner_title = get_field('banner_title')) : ?>
+        <h1 class="text-64 mb-24 leading-120"><?php echo esc_html($banner_title); ?></h1>
+      <?php endif; ?>
+      <!-- !! banner title -->
+
+      <!-- banner description -->
+      <?php if ($banner_description = get_field('banner_description')) : ?>
+        <p><?php echo $banner_description; ?></p>
+      <?php endif; ?>
+      <!-- !! banner description -->
     </div>
 
-    <div class="bg-background p-lg-64 p-32 general-content-box styled-list mb-32 rounded-16">
-      <h2>Community Guidelines</h2>
-      <p>Nepal Cloud Professionals is committed to providing a harassment-free environment for all participants,
-        regardless of gender, age, race, ethnicity, religion, disability, sexual orientation, or experience level. We
-        expect all members, attendees, speakers, sponsors, and organizers to adhere to the following guidelines:</p>
-      <ul>
-        <li>Be Respectful: Treat everyone with kindness and respect. Engage in constructive, on-topic discussions and
-          avoid personal attacks or derogatory language.</li>
-        <li>Be Inclusive: Welcome diverse perspectives and ensure everyone feels valued. Avoid language or behavior that
-          excludes or marginalizes others.</li>
-        <li>Be Professional: Maintain a professional demeanor in all interactions, whether in-person at events or online
-          in our Meetup group, social media, or forums.</li>
-        <li>No Harassment: Harassment, including sexual language, inappropriate images, or intimidation, is not
-          tolerated in any form.</li>
-        <li>Respect Privacy: Do not share personal information about others without consent, and respect boundaries
-          during networking or discussions.</li>
-      </ul>
+    <!-- code of conduct content -->
+    <?php if ($code_of_conduct_content = get_field('code_of_conduct_content')) : ?>
+      <div class="bg-background p-lg-64 p-32 general-content-box styled-list mb-32 rounded-16">
+        <?php echo $code_of_conduct_content; ?>
+      </div>
+    <?php endif; ?>
+    <!-- !! code of conduct content -->
 
-      <h2>Enforcement</h2>
-      <p>Nepal Cloud Professionals takes violations of our Code of Conduct seriously. If behavior contradicts our
-        guidelines, our organizers may take action, including:</p>
-      <ul>
-        <li>Warning: Issuing a verbal or written warning to address minor infractions.</li>
-        <li>Temporary Removal: Asking a participant to leave an event or online discussion temporarily.</li>
-        <li>Permanent Ban: Expelling individuals from events or the community for severe or repeated violations.</li>
-        <li>Event Cancellation: In extreme cases, canceling an event to ensure safety and compliance.All actions are at
-          the discretion of the organizing team, with the goal of maintaining a safe and inclusive environment.</li>
-      </ul>
-
-      <h2>Reporting</h2>
-      <p>If you experience or witness behavior that violates our Code of Conduct, please report it to our team
-        immediately. We are committed to addressing concerns promptly and confidentially.</p>
-      <ul>
-        <li>Contact Us: Email <a href="#">info@nepalcloudpro.org</a> with details of the incident, including date, time,
-          location, and description.</li>
-        <li>Anonymous Reporting: Use our anonymous reporting form for sensitive concerns (if available).</li>
-        <li>In-Person Reporting: At events, approach any organizer or staff member, identifiable by their badges. Our
-          team will review reports within 24 hours and take appropriate action, ensuring confidentiality and respect for
-          all parties involved.</li>
-      </ul>
-
-    </div>
-
+    <!-- company details. -->
     <div class="pb-64 pb-lg-96">
-      <p class="mb-4">Nepal Cloud Professional Private Limited.</p>
-      <p class="mb-4">Jawalakhel, Lalitpur, Kathmandu, 44600, NP</p>
-      <p class="mb-4">+977 9819947123</p>
-      <p class="mb-24">info@nepalcloudpro.org</p>
 
-      <p>Last Revised: 21 August, 2025</p>
+      <?php if ( $company_name = get_field( 'company_name' ) ) : ?>
+      <p class="mb-4"><?php echo esc_html( $company_name ); ?></p>
+      <?php endif; ?>
+
+      <?php if ( $company_address = get_field( 'company_address' ) ) : ?>
+      <p class="mb-4"><?php echo esc_html( $company_address ); ?></p>
+      <?php endif; ?>
+
+      <?php if ( $company_phone_number = get_field( 'company_phone_number' ) ) : ?>
+      <p class="mb-4"><?php echo esc_html( $company_phone_number ); ?></p>
+      <?php endif; ?>
+
+      <?php if ( $company_email_address = get_field( 'company_email_address' ) ) : ?>
+      <p class="mb-24"><?php echo $company_email_address; ?></p>
+      <?php endif; ?>
+
+      <p>Last Revised: <?php echo get_the_modified_date('j F, Y'); ?></p>
     </div>
+    <!-- !! company details -->
 
     <?php
     get_template_part('template-parts/cta', null);
--- wp-content/themes/ncp/template-parts/cta.php
+++ wp-content/themes/ncp/template-parts/cta.php
@@ -1,23 +1,56 @@
 <div class="cloud-cta-inner px-xl-96 py-xl-80 px-sm-52 px-40 py-40 rounded-32">
   <div class="row justify-content-between">
     <div class="col-lg-5">
-      <div class="section-title mb-28">
-        <h2 class="text-32">Become Part of Nepal Cloud Professionals</h2>
-      </div>
+      <!-- cta title -->
+      <?php if ($cta_title = get_field('cta_title')) : ?>
+        <div class="section-title mb-28">
+          <h2 class="text-32"><?php echo esc_html($cta_title); ?></h2>
+        </div>
+      <?php endif; ?>
+      <!-- !! cta title -->
+
       <div class="d-flex gap-16 flex-wrap">
-        <a href="#"
-          class="py-12 px-36 bg-primary-light text-white hover-bg-white hover-text-primary-light rounded-48 text-center">Join
-          Now</a>
-        <a href="#" class="border-white hover-border-primary-light text-white py-12 px-36 rounded-48 text-center">Attend
-          an
-          Event</a>
+        <!-- cta button 1 -->
+        <?php
+        $link = get_field('cta_button_1');
+        if ($link) :
+          $link_url = $link['url'];
+          $link_title = $link['title'];
+          $link_target = $link['target'] ? $link['target'] : '_self';
+        ?>
+          <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"
+            class="py-12 px-36 bg-primary-light text-white hover-bg-white hover-text-primary-light rounded-48 text-center"><?php echo esc_html($link_title); ?>
+          </a>
+        <?php endif; ?>
+        <!-- !! cta button 1 -->
+
+        <!-- cta button 1 -->
+        <?php
+        $link = get_field('cta_button_2');
+        if ($link) :
+          $link_url = $link['url'];
+          $link_title = $link['title'];
+          $link_target = $link['target'] ? $link['target'] : '_self';
+        ?>
+
+          <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>"
+            class="border-white hover-border-primary-light text-white py-12 px-36 rounded-48 text-center"><?php echo esc_html($link_title); ?>
+          </a>
+        <?php endif; ?>
+        <!-- cta button 1 -->
+
       </div>
     </div>
-    <div class="col-lg-6">
-      <div class="general-content-box text-white">
-        <p>Ready to grow your cloud skills and network with industry leaders? Join our community to access exclusive events, mentorship from Microsoft MVPs, and hands-on learning opportunities. Whether you’re a beginner or a seasoned pro, there’s a place for you.</p>
+    <!-- cta description -->
+    <?php if ($cta_description = get_field('cta_description')) : ?>
+      <div class="col-lg-6">
+        <div class="general-content-box text-white">
+          <p><?php echo $cta_description; ?></p>
+        </div>
       </div>
-    </div>
+    <?php endif; ?>
+    <!-- !! cta description -->
+
   </div>
 
 </div>
