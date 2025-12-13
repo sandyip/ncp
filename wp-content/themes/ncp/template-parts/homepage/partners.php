d1d82b2fb7585344f76b845cd3f1ff9242342be5
sandyip <sandipyadav0556@gmail.com>
2025-12-13 14:15:05 +0545

    Modified code in partners logo section in front-page file

--- wp-content/themes/ncp/template-parts/homepage/partners.php
+++ wp-content/themes/ncp/template-parts/homepage/partners.php
@@ -6,22 +6,55 @@
       </div>
     <?php endif; ?>
 
-    <?php if (have_rows('add_partners')) : ?>
+    <!-- <?php //if (have_rows('add_partners')) : 
+          ?>
       <div class="partners-slider">
-        <?php while (have_rows('add_partners')) :
-          the_row(); ?>
+        <?php //while (have_rows('add_partners')) :
+        //the_row(); 
+        ?>
           <?php
-          $image = get_sub_field('image');
-          $url = get_sub_field('url');
-          if ($image && $url) : ?>
+          // $image = get_sub_field('image');
+          // $url = get_sub_field('url');
+          // if ($image && $url) : 
+          ?>
 
-            <a href="<?php echo esc_url($url); ?>" class="companies-card">
+            <a href="<?php //echo esc_url($url); 
+                      ?>" class="companies-card">
 
-              <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" class="img-fluid">
+              <img src="<?php //echo esc_url($image['url']); 
+                        ?>" alt="<?php //echo esc_attr($image['alt']); 
+                                                                      ?>" class="img-fluid">
             </a>
 
-          <?php endif; ?>
-        <?php endwhile; ?>
+          <?php //endif; 
+          ?>
+        <?php //endwhile; 
+        ?>
+      </div>
+    <?php //endif; 
+    ?> -->
+
+    <?php
+    $args = array(
+      'post_type'      => 'our_partners',
+      'posts_per_page' => -1,
+      'orderby'        => 'date',
+      'order'          => 'DESC'
+    );
+
+    $partners = new WP_Query($args);
+
+    if ($partners->have_posts()) : ?>
+      <div class="partners-slider">
+        <?php while ($partners->have_posts()) : $partners->the_post();
+          $link_url = get_field('link_url');
+        ?>
+
+          <a href="<?php echo esc_html($link_url); ?>" class="companies-card">
+            <img src="<?php echo esc_url(get_the_post_thumbnail_url()); ?>" alt="<?php echo esc_attr(get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true)); ?>" class="img-fluid">
+          </a>
+        <?php endwhile;
+        wp_reset_postdata(); ?>
       </div>
     <?php endif; ?>
   </div>
